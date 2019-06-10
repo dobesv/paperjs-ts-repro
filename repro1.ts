6edbfecfb466645ba4b6eccb505bffb977a0c8c2
Dobes Vandermeer <dobesv@gmail.com>
2019-06-10 16:28:31 -0700

    Reproduce typescript error

---
+++ repro1.ts
@@ -0,0 +1,8 @@
+import * as paper from 'paper';
+
+const scope: paper.PaperScope = new paper.PaperScope();
+
+const point = new scope.Point();
+const rect = new scope.Rectangle();
+const path = new scope.Path();
+

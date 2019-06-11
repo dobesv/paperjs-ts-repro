d8279b771bda3c8c2037bd404b1ade4a9fea5638
Dobes Vandermeer <dobesv@gmail.com>
2019-06-10 17:02:59 -0700

    Add fillColor and null color cases

--- strokecolor-string.ts
+++ strokecolor-string.ts
@@ -2,5 +2,8 @@ import * as paper from 'paper';
 
 const line: paper.Path = new paper.Path();
 
+line.fillColor = 'rgb(255,255,255)';
 line.strokeColor = 'rgb(255,255,255)';
 
+line.fillColor = null;
+line.strokeColor = null;
